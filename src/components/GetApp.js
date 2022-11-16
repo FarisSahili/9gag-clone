794fc3a61597d48d8fd24659b1b42484c0f42983
FarisSahili <s12115935@stu.najah.edu>
2022-11-16 03:31:50 +0200

    ---

--- src/components/GetApp.js
+++ src/components/GetApp.js
@@ -4,8 +4,6 @@ const GetApp = () => {
     return (            
         
     <div>
-
-        <img src="//images.squarespace-cdn.com/content/v1/599501b0ff7c50ab01b0e9a1/1503023420657-4B6U70Z25EV0QCRC2KY4/Group.png?format=1500w" alt="9GAG" class="Header-branding-logo snipcss0-0-0-1 snipcss-oN2o5"/>
             <ul className="nav justify-content-end">
             <li className="nav-item">
                 <a className="nav-link disabled">FAQ</a>
