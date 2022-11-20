98e938d5beb5c6949a1f1304f2425a0c5311efd0
صبارة\CRC <s12219823@stu.najah.edu>
2022-11-20 02:22:28 +0200

    change css for card and finish my task

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import './App.css';
-import Post from './components/Post';
 
 
 import Navbar from './components/Navbar';
-
+import Post from './components/Post'
 import Scroll from './components/Scroll';
-import Logincard from './components/Logincard';
+import Logincard from './components/LoginCard';
 
 function App() {
   return (
-
 <>
  <Navbar />
  <Scroll /> 
-    <Post/>
 <Logincard />
-    <Post/>
+<Post/>
 </>
   );
 }
