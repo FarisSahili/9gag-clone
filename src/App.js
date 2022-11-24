90d38e19515d421654f1bc649ba2995ed074bc70
FarisSahili <116806358+FarisSahili@users.noreply.github.com>
2022-11-24 21:19:29 +0200

    I process for conflict

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import './App.css';
-import Logincard from './components/Logincard';
-import Modal from './components/Modal';
-import Navbar from './components/Navbar';
-import Post from './components/Post';
-import Scroll from './components/Scroll';
-import TypeButton from './components/TypeButton';
 
 function App() {
 return (
-<>
-<Modal/>
-</>
+  
+ <section>
+</section>
+  
 );
+  
 }
 
 export default App;
