fe254cd42a019a4e383a97eeb0ae8ce111bf6e40
Ibrahem Herzallah <ibrahemherzallah@gmail.com>
2022-11-26 00:36:46 +0300

    add dropdown

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import './App.css';
-
-
 import Navbar from './components/Navbar';
-import Post from './components/Post'
-import Scroll from './components/Scroll';
-import Logincard from './components/LoginCard';
+
+
 
 function App() {
   return (
-<>
  <Navbar />
- <Scroll /> 
-<Logincard />
-<Post/>
-</>
+
   );
 }
 
