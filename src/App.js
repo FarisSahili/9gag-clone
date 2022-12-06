8581f3a6192d787e9104819191f6ea36d7545b33
Ibrahem Herzallah <ibrahemherzallah@gmail.com>
2022-12-06 15:13:34 +0300

    I make some changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import Navbar from './components/Navbar';
-import Profile from './components/Profile';
-
-// import Navbar from './components/Navbar';
 function App() 
 {
   return (
   <section>
+
   </section>
   );
 }
--- src/components/N.js
+++
@@ -1,2 +0,0 @@
-//let termm=document.getElementById("term");
-console.log("dsdsflmfdspfm") 
