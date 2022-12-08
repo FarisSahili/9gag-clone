81a1e1b7ff0e52ee589d2ec400771ca162363664
Ibrahem Herzallah <ibrahemherzallah@gmail.com>
2022-12-08 04:52:00 +0300

    finish responsive of profile page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import Navbar from './components/navbarhome/Navbar';
+// import PostCard from './components/PostCard/PostCard';
+import Profile from './components/profilepage/Profile';
 function App() 
 {
   return (
 
-
-<section></section>
-
+<section>
+  <Navbar />
+  <Profile />
+  {/* <PostCard />   */}
+</section>
 
      );
 }
