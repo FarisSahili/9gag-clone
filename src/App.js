25b0fbe1cfa0141a720231aa07ef0dc6a4952cef
FarisSahili <s12115935@stu.najah.edu>
2022-12-08 21:18:11 +0200

    I made some changes to the Package.json file

--- src/App.js
+++ src/App.js
@@ -1,31 +1,7 @@
 import './App.css';
-<<<<<<< HEAD
-import CommentPage from './components/CommentPage';
-=======
-
-
-
-
->>>>>>> c9cc3ec6d4485d68e1b4f9818eab3832ec548d37
-function App() 
-{
-
-
-
-  return (
-<<<<<<< HEAD
-  <section>
-    <CommentPage/>
-  </section>
-  );
-=======
-
-
+function App() {
+return (
 <section></section>
-
-
-     );
->>>>>>> c9cc3ec6d4485d68e1b4f9818eab3832ec548d37
+);
 }
 export default App;
-
