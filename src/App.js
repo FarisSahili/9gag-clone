147554022bec700f9a745d0ad6eab3d1b6aea027
صبارة\CRC <s12219823@stu.najah.edu>
2022-12-09 02:36:48 +0200

    Make Scroll Responsive

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
 import './App.css';
-import Navbar from './components/Navbar';
-import PostCard from './components/PostCard/PostCard';
-import LoginCard from './components/loginCard/LoginCard';
-import Scroll from './components/Scroll';
-import SideBar from './components/sidebar/Sidebar'
-import { Modal } from 'react-bootstrap';
+
 function App() 
 {
   return (
-  // <section>
-  // </section>
-  <>
-  <Navbar/>
-<Scroll/>
-<TypeButton/>
-<PostCard/>
-<Modal/>
-  </>
- 
+   <section></section>
   );
 }
 export default App;
---
+++ src/components/tagbutton/tagButton.js
@@ -0,0 +1,33 @@
+
+import './tagButton.css';
+
+//function
+
+const TagButton = () => {
+return (
+    <div className='TagCont'>
+ 
+                                <a  className='Tag-Button'  href = "tag-html" >black panther</a>
+                                <a  className='Tag-Button'  href = "tag-html" >Wakanda forever</a>
+                                <a  className='Tag-Button'  href = "tag-html">pokemon</a>
+                                <a  className='Tag-Button'  href = "tag-html" >ftx</a>
+                                <a  className='Tag-Button'  href = "tag-html" >Elon musk</a>
+                                <a  className='Tag-Button'  href = "tag-html" >twitter</a>
+        <br/>
+
+                                    <a className='Tag-Button' href = "tag-html">no nut november</a>
+                                    <a className='Tag-Button' href = "tag-html">house of the dragon</a>
+                                    <a className='Tag-Button' href = "tag-html">chainsaw man</a>        
+                                    <a className='Tag-Button' href = "tag-html">latest news</a>
+        <br/>
+                                <a className='Tag-Button' href = "tag-html">most commented</a>
+                                <a className='Tag-Button' href = "tag-html">weekly highlights</a>
+                                </div>
+);
+
+
+}
+
+//Export
+export default TagButton;
+
