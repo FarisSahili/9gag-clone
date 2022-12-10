924203ea9cef37856ae74bd874f32e215fcd65ff
unknown <[s12028365@stu.najah.stu]>
2022-12-11 00:20:39 +0200

    final push

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 
 import './App.css';
+import Posts from './components/Posts';
  
- 
- 
-
 function App() {
   return (
     <section>
- 
+    <Posts/>
     </section>
   );
 
--- src/components/ModalTags.js
+++ src/components/ModalTags.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./ModalTags.css";
- 
+
 const Star = ({ isSelected, onClick }) => {
   return (
     <div className="start">
@@ -10,7 +10,7 @@ const Star = ({ isSelected, onClick }) => {
           fill={isSelected ? "gold" : "gray"}
           className="bi bi-star-fill"
           width="20"
-           height="20"
+          height="20"
           viewBox="0 0 16 16"
         >
           <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
@@ -39,7 +39,7 @@ const XButton = ({ isSelected, onClick }) => {
 };
 
 function ModalTags() {
- 
+
   const [popularTags, setPopularTags] = useState([
     "9gag",
     "china",
@@ -48,7 +48,7 @@ function ModalTags() {
     "german",
     "qatar 2022",
   ]);
- 
+
   const [otherTags, setOtherTags] = useState([
     "anime",
     "girl",
@@ -67,144 +67,144 @@ function ModalTags() {
     "fortnite",
     "girl celebrity",
   ]);
-  
+
   const [favorites, setFavorites] = useState([]);
   const [hidden, setHidden] = useState([]);
- 
-
 
   const filterArrayFromFavorite = (selectedIndex) => {
-    setPopularTags((current) =>current.filter((item, index) => index !== selectedIndex)
+    setPopularTags((current) => current.filter((item, index) => index !== selectedIndex)
     );
   };
-  
+
   const deleteItemFromFavorite = (selectedIndex) => {
-    setFavorites((current) =>current.filter((item, index) => index !== selectedIndex)
+    setFavorites((current) => current.filter((item, index) => index !== selectedIndex)
     );
   };
-  
+
   const filterArrayFromOtherFavorite = (selectedIndex) => {
-    setOtherTags((current) =>current.filter((item, index) => index !== selectedIndex)
+    setOtherTags((current) => current.filter((item, index) => index !== selectedIndex)
     );
   };
   const filterArrayFromhidden = (selectedIndex) => {
-    setHidden((current) =>current.filter((item, index) => index !== selectedIndex)
+    setHidden((current) => current.filter((item, index) => index !== selectedIndex)
     );
   };
- 
-
+  
   return (
     <>
-     <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-   modal
-</button>
-     <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        modal
+      </button>
+      <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div className="modal-dialog">
-        <div className="modal-content">
-         <div className="modal-header">
-        <h5 className="modal-title">Customize with Tags</h5>
-        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-         </div>
-          <div className="modal-body">
-          <span>Favorites</span>
-          {
-          favorites.map((item, index) => {
-            return (
-              <div className="rowContainer">
-                <div className="title">{item}</div>
-                <div className="btnsContainer">
-                  <Star isSelected={true} 
-                   onClick={() => {
-                    deleteItemFromFavorite(index);
-                    setPopularTags((current) => [...current, item]);
-                  }}
-                  />
-                 
-                </div>
-              </div>
-            );
-          })}
-        </div>
-        <div className="modal-body">
-          <span>Explore Popular Tags</span>
-          <ul className="list-group list-group-flush">
-            {popularTags.map((item, index) => {
-              return (
-                <div className="rowContainer">
-                  <div className="title">{item}</div>
-                  <div className="btnsContainer">
-                  <XButton
-                      onClick={() => {
-                        filterArrayFromFavorite(index);
-                        setHidden((current) => [...current, item]);
-                      }}
-                    />
-                    <Star
-                      onClick={() => {
-                        filterArrayFromFavorite(index);
-                        setFavorites((current) => [...current, item]);
-                      }}
-                    />
-                  
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">Customize with Tags</h5>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+            
+              
+              <span>Favorites</span>
+               {
+                favorites .map((item, index) => {
+                  return (
+                    <div className="rowContainer">
+                      <div className="title">{item}</div>
+                      <div className="btnsContainer">
+                        <Star isSelected={true}
+                          onClick={() => {
+                            deleteItemFromFavorite(index);
+                            setPopularTags((current) => [...current, item]);
+                          }}
+                        />
+
+                      </div>
+                    </div>
+                  );
+                })}
+            </div>
+            <div className="modal-body">
+              <span>Explore Popular Tags</span>
+              <ul className="list-group list-group-flush">
+                {popularTags.map((item, index) => {
+                  return (
+                    <div className="rowContainer">
+                      <div className="title">{item}</div>
+                      <div className="btnsContainer">
+                        <XButton
+                          onClick={() => {
+                            filterArrayFromFavorite(index);
+                            setHidden((current) => [...current, item]);
+                          }}
+                        />
+                        <Star
+                          onClick={() => {
+                            filterArrayFromFavorite(index);
+                            setFavorites((current) => [...current, item]);
+                          }}
+                        />
+
+                      </div>
+                    </div>
+                  );
+                })}
+              </ul>
+            </div>
+            <hr></hr>
+            <div className="modal-body">
+              <span>Other Tags</span>
+              <ul className="list-group list-group-flush">
+                {otherTags.map((item, index) => {
+                  return (
+                    <div className="rowContainer">
+                      <div className="title">{item}</div>
+                      <div className="btnsContainer">
+                        <XButton
+                          onClick={() => {
+                            filterArrayFromFavorite(index);
+                            setHidden((current) => [...current, item]);
+                          }}
+                        />
+                        <Star
+                          onClick={() => {
+                            filterArrayFromOtherFavorite(index);
+                            setFavorites((current) => [...current, item]);
+                          }}
+                        />
+
+                      </div>
+                    </div>
+                  );
+                })}
+              </ul>
+            </div>
+            <hr></hr>
+            <div className="modal-body">
+              <span>Hidden</span>
+              {hidden.map((item, index) => {
+                return (
+                  <div className="rowContainer">
+                    <div className="title">{item}</div>
+                    <div className="btnsContainer">
+
+                      <XButton isSelected={true}
+                        onClick={() => {
+                          filterArrayFromhidden(index);
+                          setFavorites((current) => [...current, item]);
+                        }}
+                      />
+                    </div>
                   </div>
-                </div>
-              );
-            })}
-          </ul>
+                );
+              })}
+              <p>Posts from these tags will be filtered from Home page.</p>
+            </div>
+          </div>
         </div>
-        <hr></hr>
-        <div className="modal-body">
-          <span>Other Tags</span>
-          <ul className="list-group list-group-flush">
-            {otherTags.map((item, index) => {
-              return (
-                <div className="rowContainer">
-                  <div className="title">{item}</div>
-                  <div className="btnsContainer">
-                  <XButton
-                      onClick={() => {
-                        filterArrayFromFavorite(index);
-                        setHidden((current) => [...current, item]);
-                      }}
-                    />
-                    <Star
-                      onClick={() => {
-                        filterArrayFromOtherFavorite(index);
-                        setFavorites((current) => [...current, item]);
-                      }}
-                    />
-                   
-                  </div>
-                </div>
-              );
-            })}
-          </ul>
-        </div>
-        <hr></hr>
-        <div className="modal-body">
-          <span>Hidden</span>
-          {hidden.map((item, index) => {
-            return (
-              <div className="rowContainer">
-                <div className="title">{item}</div>
-                <div className="btnsContainer">
-                  
-                  <XButton isSelected={true}
-                   onClick={() => {
-                    filterArrayFromhidden(index);
-                    setFavorites((current) => [...current, item]);
-                  }} 
-                  />
-                </div>
-              </div>
-            );
-          })} 
-           <p>Posts from these tags will be filtered from Home page.</p>
-        </div>
-      </div>
-      </div>
       </div>
     </>
   );
 }
+  
 export default ModalTags;
