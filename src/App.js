719cda81431dc763d375a0ba4f0037f804e50881
unknown <[s12028365@stu.najah.stu]>
2022-12-11 19:30:06 +0200

    edit modal tags

--- src/App.js
+++ src/App.js
@@ -1,35 +1,14 @@
 
 import './App.css';
-<<<<<<< HEAD
  
- 
-function App() {
-  return (
-    <section>
- 
-    </section>
-  );
-
-=======
-
-
-
-
-
-
-
-
 function App() 
 {
-
-
-
   return (
 
-
-<section></section>
+<section>
+ 
+</section>
 );
->>>>>>> 9ba6e5447269d21889aaf2377cd8edcfb7eb3312
 }
 
 export default App;
