8118d41ddfe3a3ee8008bc0c85bbf116357602d8
صبارة\CRC <s12219823@stu.najah.edu>
2022-12-13 00:51:10 +0200

    post card & dummy data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 
 
 import './App.css';
-import PostCard from './components/postcard/PostCard';
-import posts from './components/Postdata' ;
 
 
 
 function App() 
 {  return (
-        <>
-    {posts.map((post) => {
-        return <PostCard key={post.name} username={post.op_user} avatar={post.op_user_avatar} title={post.title} media={post.content}
-            tag={post.tags}
-            up={post.up_votes} down={post.down_votes} time={post.post_time}
-        />
-    })}
-    </> 
-    )
-    
-;
+
+<>
+</>
+)
 }
 export default App;
