d0476ec3ecfe3395d4dce1310172713c91f45071
unknown <[s12028365@stu.najah.stu]>
2022-12-12 18:12:37 +0200

    add dummy data

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 
 
 import './App.css';
- import  ModalTags from './components/ModalTags';
- import RecommendedPosts from './components/Shuffle'
+ 
 function App() 
 {
   return (
     
 <section>
- <ModalTags/>
- <RecommendedPosts/>
-  
+ 
 </section>
 );
 }
