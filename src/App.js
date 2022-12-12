c0442e6ef8b6356f09e1ef5e5cb6f80bc0967c54
FarisSahili <s12115935@stu.najah.edu>
2022-12-12 18:20:03 +0200

    I made some changes for comment page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 
 import './App.css';
+import CommentPage from './components/commentpage/CommentPage';
+import Navbar from './components/navbarhome/Navbar';
+import Posts from './components/Posts';
+
 function App() 
 {
 return (
 <>
+    <Navbar/>
+    <Posts/>
+    <CommentPage/>
 </>
+
 );
 }
 export default App;
