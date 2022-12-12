24cc5897650ac5268993497f78253bdb0c80adae
Basil0X7 <116971576+Basil0X7@users.noreply.github.com>
2022-12-12 20:28:13 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 
 
 import './App.css';
-
-import Newpost_page from './components/Newpostt/Newpost_page';
-
-
-
-
 function App() 
-{  return (
-
-<section>
-  <Newpost_page />
-</section>
+{
+return (
+<>
+</>
 );
 }
-
 export default App;
