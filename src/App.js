8ae744495bb207b3af43269a7c45a00cdf309102
صبارة\CRC <s12219823@stu.najah.edu>
2022-12-13 23:04:51 +0200

    add file for up & down vote buttons & create up vote and down vote buttons  with JS

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 
 
 import './App.css';
-
-
+import Postdata from './components/Postdata'
+import Count from './components/postcard/count'
+import bookmark from './components/postcard/photo-post-card/bookmark'
 
 function App() 
 {  return (
-
 <>
+<Count/>
+<bookmark/>
 </>
-)
+);
 }
 export default App;
--- src/components/PostCard/count.js
+++ src/components/PostCard/count.js
@@ -1,16 +1,38 @@
-const handleClick =()=>{
-    const [count, setCount] = useState(100);
+// const handleClick =()=>{
+//     const [count, setCount] = useState(100);
 
-  setCount(current => current + 1);
-};
+//   setCount(current => current + 1);
+// };
+ 
+import { useState } from 'react';
+import votedown from './photo-post-card/votedown.ico';
 
+import voteup from './photo-post-card/voteup.ico';
 
+const Upvotebutton = () =>{
+// let number = '100';
+const [number,setNumber] = useState(100);
+const [numberd,setNumberd] = useState(101);
 
-{/* <>
-{posts.map((post) => {
-    return <PostCard key={post.name} username={post.op_user} avatar={post.op_user_avatar} title={post.title} media={post.content}
-        tag={post.tags}
-        up={post.up_votes} down={post.down_votes} time={post.post_time}
-    />
-})}
-</> */}
+    const Upvote =()=>{
+        // number=101
+        // console.log(number)
+        setNumber(current =>current +1)
+    }
+    
+    const Downvote =()=>{
+        // number=101
+        // console.log(number)
+        setNumberd(current =>current -1)
+    }
+    
+    return (
+
+  <div>    
+        <button onClick={Upvote} className='button-vote'><img src = {voteup} alt="this is vote up icon"/>{number}</button>
+        <button  onClick={Downvote} className='button-vote'><img src = {votedown} alt ="this is vote down icon "/>  {numberd}</button>
+
+   </div>
+    )
+}
+export default Upvotebutton;
