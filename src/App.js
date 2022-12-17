059b3faa63f628f8ef31bb0919b2324e44222448
Ibrahem Herzallah <ibrahemherzallah@gmail.com>
2022-12-17 23:22:48 +0300

    finish transfer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 
  
 import './App.css';
+import Profile from './components_new/Pages/Profile';
+import Navbar from './components_new/UI/Navbar';
 
 function App() 
 {
 return (
     
-<>
-</>
+    <>
+        <Navbar />
+        <Profile />
+    </>
 
 );
 }
