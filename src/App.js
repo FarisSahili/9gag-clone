058be53982284c2090dbab48e551803d7e25ebb3
صبارة\CRC <s12219823@stu.najah.edu>
2022-12-17 22:32:55 +0200

    start with routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 
  
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css'; 
 //import CustomTags from '../src/components_new/UI/CustomizeTags/CustomTags.js'<CustomTags/>
 import RecommendedPosts from './components_new/Pages/Shuffle';
+import PostList from './components_new/Posts/PostList';
 
 function App() 
 {
   return (
-   
-<section>
-<RecommendedPosts/>
-</section>
+   <BrowserRouter>
+       <Routes>
+        <Route path="/Pages/Shuffle" element={RecommendedPosts}/>
+
+       </Routes>
+   </BrowserRouter>
 );
 }
 
