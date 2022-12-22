e755990e7b57c04e7dd1b34173e38cfd24c24f7b
Ibrahem Herzallah <ibrahemherzallah@gmail.com>
2022-12-22 03:20:06 +0300

    edit navbar

--- src/components/UI/Navbar.js
+++ src/components/UI/Navbar.js
@@ -12,46 +12,6 @@ const itemClicked = (event) => {
 
 const Navbar = () => {
 
-
-/*import './Navbar.css';
-
-
-
-
-const Navbar = () => {
-
-
-    const itemClicked = (event) => {
-        event.stopPropagation();
-    }
-   
-    /*const dropdowns=document.querySelectorAll('.Dropdown');
-    dropdowns.forEach(Dropdown=>{
-    const select=Dropdown.querySelector('.select');
-    const caret=Dropdown.querySelector('.caret');
-    const menu=Dropdown.querySelector('.menu');
-    const options=Dropdown.querySelectorAll('.menu li');
-    const selected=Dropdown.querySelector('.test');
-    select.addEventListener('click', () =>{
-     
-        select.classList.toggle('select-clicked');
-        caret.classList.toggle('caret-rotate');
-        menu.classList.toggle('menu-open');
-    })
-    options.forEach(option => {
-      option.addEventListener('click',() => {
-         test.innerText = option.innerText;
-         select.classList.remove('select-clicked');
-         caret.classList.remove('caret-rotate');
-         menu.classList.remove('menu-open');
-         options.forEach(option => {
-           option.classList.remove('active');
-         });
-    option.classList.add('active');
-      });
-     });
-    });*/
-
    
     return(        
           <div className='basic-navbar sticky-top'>
