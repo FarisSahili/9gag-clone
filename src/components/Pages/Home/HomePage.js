ca9e692cdacf2f086acc3e3c7854c8dd0904fe86
ibrahim-Khdair <ibraheem.khdier@gmail.com>
2022-12-24 10:30:44 +0200

    final task to finsh tha work

--- src/components/Pages/Home/HomePage.js
+++ src/components/Pages/Home/HomePage.js
@@ -14,7 +14,7 @@ const HomePage = (props) => {
         
             <Navbar/>
             <ModalTags/>
-            <SocialMediaSignup/> 
+            <SocialMediaSignup/>           
             <Interests/>
             <TagButton/>
             <PostList/>   
