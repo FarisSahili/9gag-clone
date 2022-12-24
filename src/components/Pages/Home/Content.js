4b06d48555d621911f91d57c59fe62590704975a
صبارة\CRC <s12219823@stu.najah.edu>
2022-12-24 22:11:14 +0200

    Finalllly thae last commit , push !

--- src/components/Pages/Home/Content.js
+++ src/components/Pages/Home/Content.js
@@ -1,13 +1,7 @@
 import PostList from "../../Posts/PostList";
-import SocialMediaSignup from "../../SignupLoginModal/SocialMediaSignup";
-import ModalTags from "../../UI/CustomizeTags/CustomTags";
-import Navbar from "../../UI/Navbar";
-import Sidebar from "../../UI/Sidebar/Sidebar";
 import Interests from "./Interests";
-import RecommendedPosts from "../Shuffle";
 import TagButton from "../../Tags/Tag";
-import Logincard from "../../Posts/LoginPost";
-import Dark from "../../../dark";
+
 import homegag from './homegag.png'
 import './Content.css'
 const Content = (props) => {
--- src/components/Pages/Home/HomePage.js
+++ src/components/Pages/Home/HomePage.js
@@ -1,25 +1,20 @@
-import PostList from "../../Posts/PostList";
 import SocialMediaSignup from "../../SignupLoginModal/SocialMediaSignup";
 import ModalTags from "../../UI/CustomizeTags/CustomTags";
 import Navbar from "../../UI/Navbar";
 import Sidebar from "../../UI/Sidebar/Sidebar";
-import Interests from "./Interests";
-import RecommendedPosts from "../Shuffle";
-import TagButton from "../../Tags/Tag";
 import Logincard from "../../Posts/LoginPost";
-import Dark from "../../../dark";
 import Content from "./Content";
 const HomePage = (props) => {
     return (
         <>
             <Navbar/>
             <Sidebar/>
-            <Dark/>
             <aside>
             <SocialMediaSignup/> 
             <ModalTags/> 
             </aside>
             <Content/>
+            <Logincard/>
         </>
     );
 }
