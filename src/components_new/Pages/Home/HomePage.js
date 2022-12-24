c6906b2535cf919430bfb1dca81c8ba9ee399aa8
Ibrahem Herzallah <ibrahemherzallah@gmail.com>
2022-12-24 03:23:40 +0300

    update at dropdown

---
+++ src/components_new/Pages/Home/HomePage.js
@@ -0,0 +1,11 @@
+const HomePage = (props) => {
+    return(
+        <>
+            <Navbar />
+            <Sidebar />
+            <h1>This is the HomePage</h1>
+        </>
+    );
+}
+
+export default HomePage;
